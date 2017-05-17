2061c8418fed14846fe52cf9eeddc84725fd17e0
atguiguafu <541433511@qq.com>
2017-05-17 16:00:25 +0800

    nihao

--- app/src/main/java/svntest/androidstudio0224/MainActivity.java
+++ app/src/main/java/svntest/androidstudio0224/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("nihao");
+        System.out.println("nihao");
     }
 }
